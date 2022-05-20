ca125498aad007fd53415c483cfd1d07dd79b8d3
Mocking_jayk0918 <jesper2able@gmail.com>
2022-05-20 17:48:30 +0900

    1.0(codeArrange 22.05.20 17:48)

--- src/com/javaex/ConnectTest_01.java
+++ src/com/javaex/ConnectTest_01.java
@@ -12,29 +12,28 @@ public class ConnectTest_01 {
 		String url = "jdbc:oracle:thin:@webdb_high?TNS_ADMIN=/Users/jaykim0918/Dropbox/Wallet_webdb";
 		String userid = "admin";
 		String pwd = "Jayk09180918";
-		
+
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		
-		//oracle DB연결 드라이버 로딩
+
+		// oracle DB연결 드라이버 로딩
 		try {
 			Class.forName("oracle.jdbc.OracleDriver");
 			System.out.println("JDBC 드라이버 로딩 성공");
-			}
-		catch(ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) {
 			System.out.println("error : 드라이버 로딩 실패 - " + e);
-			}
-		
-		//DBMS와 연결
+		}
+
+		// DBMS와 연결
 		try {
 			System.out.println("DB 연결 준비......");
 			conn = DriverManager.getConnection(url, userid, pwd);
-			
-			if(conn != null) {
+
+			if (conn != null) {
 				System.out.println("DB 연결 성공...");
 			}
-			
+
 			try {
 				// SQL문 준비
 				String query = "";
@@ -43,35 +42,35 @@ public class ConnectTest_01 {
 				pstmt = conn.prepareStatement(query);
 				// pstmt.setString(0, query);
 				// pstmt.setInt(0, 0);
-				
+
 				// 실행
 				int count = pstmt.executeUpdate();
-				
+
 				// 결과처리보고
 				System.out.println(count + "건이 등록되었습니다.");
-				
-			}catch(SQLException e) {
+
+			} catch (SQLException e) {
 				System.out.println("error:" + e);
-			}finally {
+			} finally {
 				try {
 					// 자원 정리
-	        		if (rs != null) {
-	        			rs.close();
+					if (rs != null) {
+						rs.close();
 					}
-	        		if (pstmt != null) {
+					if (pstmt != null) {
 						pstmt.close();
 					}
-	        		if (conn != null) {
+					if (conn != null) {
 						conn.close();
 					}
-				}catch (SQLException e) {
+				} catch (SQLException e) {
 					System.out.println("error:" + e);
 				}
 			}
-			
-		}catch(SQLException e) {
+
+		} catch (SQLException e) {
 			System.out.println("error" + e);
 		}
-       
+
 	}// main end
 }// class end
