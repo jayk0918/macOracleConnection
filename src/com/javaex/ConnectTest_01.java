b756d694b2fa223efed59bcfb18ee5a04d9b3146
Mocking_jayk0918 <jesper2able@gmail.com>
2022-05-20 13:24:26 +0900

    1.0(code_arrange 22.05.20 13:24)

--- src/com/javaex/ConnectTest_01.java
+++ src/com/javaex/ConnectTest_01.java
@@ -9,35 +9,35 @@ import java.sql.SQLException;
 /* 오라클 접속 테스트 */
 public class ConnectTest_01 {
 	public static void main(String[] args) {
-       String url = "jdbc:oracle:thin:@webdb_high?TNS_ADMIN=/Users/jaykim0918/Dropbox/Wallet_webdb";
-       String userid = "admin";
-       String pwd = "Jayk09180918";
-       
-     
-       //드라이버 로딩 
-       try {
-    	    //oracle DB연결 드라이버 로딩
-    	    Class.forName("oracle.jdbc.OracleDriver");//
-    	    System.out.println("JDBC 드라이버 로딩 성공");
-       }catch(ClassNotFoundException e) {
-   	    	System.out.println("error : 드라이버 로딩 실패 - " + e);
-		}
-       
-       //DBMS와 연결
-       try {
-	        System.out.println("DB 연결 준비......");
+		String url = "jdbc:oracle:thin:@webdb_high?TNS_ADMIN=/Users/jaykim0918/Dropbox/Wallet_webdb";
+		String userid = "admin";
+		String pwd = "Jayk09180918";
+		
+		//oracle DB연결 드라이버 로딩
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			System.out.println("JDBC 드라이버 로딩 성공");
+			}
+		catch(ClassNotFoundException e) {
+			System.out.println("error : 드라이버 로딩 실패 - " + e);
+			}
+		
+		//DBMS와 연결
+		try {
+			System.out.println("DB 연결 준비......");
 	        Connection conn =DriverManager.getConnection(url, userid, pwd);
 	        PreparedStatement pstmt = null;
             ResultSet rs = null;
-	        if(conn!=null) {
+            if(conn!=null) {
 	        	System.out.println("DB 연결 성공...");
 	        }
-	        
-	        // SQL문 준비 / 바인딩 / 실행
+            
+            // SQL문 준비 / 바인딩 / 실행
 	        // try_catch_finally문
-	        
-       }catch(SQLException e) {
-    	   System.out.println("error" + e);
-       }
+			
+		}catch(SQLException e) {
+			System.out.println("error" + e);
+		}
+       
 	}// main end
 }// class end
